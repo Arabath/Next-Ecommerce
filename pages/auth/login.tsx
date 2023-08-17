96bf9219a8daee3d7688bfcd9d35a854a5a12cb5
arabath <axelhobrecht@hotmail.com>
2023-08-17 12:36:47 -0300

    Instalacion react-hook-form para login

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,10 +1,25 @@
 import NextLink from 'next/link';
 import { Box, Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
 import { AuthLayout } from '../../components/layouts'
 
+type FormData = {
+    email: string,
+    password: string
+}
+
 const LoginPage = () => {
-  return (
+  
+    const { register, handleSubmit,  formState:{ errors } } = useForm<FormData>();
+
+    const onLoginUser = ( data: FormData ) => {
+        console.log( {data} );
+    }
+
+    return (
     <AuthLayout title={'Ingresar'}>
+        <form onSubmit={handleSubmit(onLoginUser)}>
         <Box sx={{ width: 350, padding:'10px 20px' }}>
             <Grid container spacing={2}>
                 <Grid item xs={12}>
@@ -12,14 +27,25 @@ const LoginPage = () => {
                 </Grid>
 
                 <Grid item xs={12}>
-                    <TextField label="Correo" variant="filled" fullWidth />
+                    <TextField 
+                        label="Correo" 
+                        variant="filled" 
+                        fullWidth 
+                        {...register('email')} 
+                    />
                 </Grid>
                 <Grid item xs={12}>
-                    <TextField label="Contraseña" type='password' variant="filled" fullWidth />
+                    <TextField 
+                        label="Contraseña" 
+                        type='password' 
+                        variant="filled" 
+                        fullWidth 
+                        {...register('password')}
+                    />
                 </Grid>
 
                 <Grid item xs={12}>
-                    <Button color="secondary" className='circular-btn' size='large' fullWidth>
+                    <Button color="secondary" className='circular-btn' size='large' type='submit' fullWidth>
                         Ingresar
                     </Button>
                 </Grid>
@@ -33,6 +59,7 @@ const LoginPage = () => {
                 </Grid>
             </Grid>
         </Box>
+        </form>
     </AuthLayout>
   )
 }
